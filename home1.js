05e1765c4899866a949664463ae8be765b24ba00
Yurii Pasenchenko <yurii.pasenchenko@gmail.com>
2018-10-22 19:33:25 +0300

    first commit

---
+++ home1.js
@@ -0,0 +1,21 @@
+//завдання 1
+let city;
+city = "Київ";
+city = "Львів";
+let address = city;
+alert(address);
+
+//завдання 2
+let n = 3;
+let result = Math.pow(n, 3);
+alert(result);
+
+//завдання 3
+let pen = 5.25;
+let pencil = 3.50;
+let kspen = 4;
+let kspecil = 6;
+let vartist = (pen * kspen) + (pencil * kspecil);
+alert(vartist);
+
+//завдання 4
